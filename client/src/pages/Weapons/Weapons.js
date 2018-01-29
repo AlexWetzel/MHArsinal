5c9032ecd11ef3c3c108626c2131789fc9488139
AlexWetzel <wetzela@live.com>
2018-01-29 16:51:59 -0500

    More descriptions added to the weapons page

--- client/src/pages/Weapons/Weapons.js
+++ client/src/pages/Weapons/Weapons.js
@@ -73,9 +73,88 @@ const Weapons = () =>
 			stats={[4,2,4,1,4]}
 			text={"A blunt weapon made for hitting monsters in the face, knocking them out cold. " +
 						"The hammer has good mobility and weighty, powerful attacks, letting you home in on your target to deliver stunning blows. " +
-						"Charge attacks let you access different attacks while you move, and you can store your charge to boost your power " +
+						"Charge attacks let you access different attacks while you move, and you can store your charge to boost your power. " +
 						"The hammer also has strong standing combos, making the hammer an accurate weapon on immobilized monsters. " +
 						"Aiming attacks will be the greatest hurdle to learning this weapon, as its reach is poor."
+			}
+		/>
+		<WeaponSection 
+			key={"Hunting Horn"}
+			name={"Hunting Horn"}
+			src={"./images/HH_icon.png"}
+			link={"/huntinghorn"}
+			stats={[3,2,5,1,5]}
+			text={"An instrument of music as well as carnage. " +
+						"The hunting horn's different attacks each play a note. " +
+						"When the right notes are played in order, you can recite the song, granting status boosts to your party. " +
+						"Naturally, this weapon is extremely desireable in multiplayer hunts. " +
+						"While its overall damage is lackluster, its mmobility, reach, and ability to KO monsters make this weapon easy to use."
+			}
+		/>
+		<WeaponSection 
+			key={"Lance"}
+			name={"Lance"}
+			src={"./images/Lan_icon.png"}
+			link={"/lance"}
+			stats={[3,3,2,5,2]}
+			text={"A defense-oriented weapon with slow movement and fast attacks. " +
+						"The lance was made for relentlessly targeting the monster's weak spot while using the shield to stay safe. " +
+						"Capable of guard dashing, counter thrusts, and strong omnidirectional guards, the lance boasts more defensive techniqes than any other weapon. " +
+						"The lance's dash attack allows it to close long gaps and end with a powerful thrust. " +
+						"However, the lance has trouble moving away from attacks when guarding isn't an option."
+			}
+		/>
+		<WeaponSection 
+			key={"Gunlance"}
+			name={"Gunlance"}
+			src={"./images/GL_icon.png"}
+			link={"/gunlance"}
+			stats={[4,3,1,3,1]}
+			text={"An offense-oriented lance with a wide variety of attack options. " +
+						"The gunlance not only has fast thrusts and powerful swings, but it can also fire heavy artillery from its barrel. " +
+						"From hide-piercing shells, the precise wyrmstake, and the explosive wyvern's fire, the gunlance has options for any situation " +
+						"While its shield is as strong as the lances, this weapon lacks any defensive moves, so it's often better to evade to position yourself. " +
+						"The gunlance's slow movement and complex combos make this a difficult weapon to learn."
+			}
+		/>
+		<WeaponSection 
+			key={"Switch Axe"}
+			name={"Switch Axe"}
+			src={"./images/SA_icon.png"}
+			link={"/switchaxe"}
+			stats={[4,4,2,1,2]}
+			text={"A long-reaching axe that morphs into a swift, heavy sword " +
+						"In axe mode you gradually build phial energy to be used in sword mode. " +
+						"Sword mode attacks are powerful, and get stronger as you keep attacking and achieve phial awakening. " +
+						"In this state, you can thrust your sword into a monster, grapple onto them, and unleash an explosive finish. " +
+						"Switchaxe users rely on evading and positioning to defend themselves."
+			}
+		/>
+		<WeaponSection 
+			key={"Charge Blade"}
+			name={"Charge Blade"}
+			src={"./images/CB_icon.png"}
+			link={"/chargeblade"}
+			stats={[5,3,4,4,4]}
+			text={"A heavy sword and shield that combine into a mighty axe. " +
+						"Sword mode can charge phial energy, which can be used to unleash powerful axe attacks and to strengthen itself further. " +
+					 	"Sword mode is fairly mobile, and can slide in any direction to reposition easily. " +
+						"Furthermore, you can guard during certain attacks, which let you immediately use your strongest move to punish monsters. " +
+						"Axe mode is strong, yet slow, and should be used carefully. " +
+						"The charge blade is indeed strong, but can be difficult to master."
+			}
+		/>
+		<WeaponSection 
+			key={"Insect Glaive"}
+			name={"Insect Glaive"}
+			src={"./images/IG_icon.png"}
+			link={"/insectglaive"}
+			stats={[2,5,5,1,4]}
+			text={"A long polearm paired with a kinsect companion" +
+						"The glaive is not only capable of swift attacks, but it can also be used to vault into the air, attacking mosters from above. " +
+					 	"The kinsect can be used to attack monsters, leaving status inflicting dust, and retrieve extracts. " +
+						"These extracts can be used to increase the number of hits you deal, boost your movement speed, and improve your defense. " +
+						"While this weapon requires a certain amount of upkeep, its maneuverability makes it an attractive choice for new hunters."
 			}
 		/>
 
