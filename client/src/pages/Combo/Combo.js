52e5a740b6e58044309a689c82f89e1ca2913473
AlexWetzel <wetzela@live.com>
2018-01-30 21:20:04 -0500

    changed controller api, controller support in progress

--- client/src/pages/Combo/Combo.js
+++ client/src/pages/Combo/Combo.js
@@ -4,8 +4,10 @@ import axios from "axios";
 import Attack from "./../../components/Attack";
 import Option from "./../../components/Option";
 import InfoPanel from "./../../components/InfoPanel";
-// import gamepad from "./../../utils/gamepad.js";
+import gamepad from "./../../utils/gamepad.js";
 import './Combo.css'
+
+import Gamepad from 'react-gamepad';
 
 
 
@@ -21,111 +23,122 @@ class Combo extends Component {
 
 	componentDidMount() {
     // Load articles after the state changes
+
 		this.loadMovelist();
 
-		// this.gamepadListener();
-		// this.checkGamepad();
-		this.gamepad();
 
 		// window.addEventListener("gamepadconnected", gamepadAPI.connect);
 		// window.addEventListener("gamepaddisconnected", gamepadAPI.disconnect);
   }
+
+  connectHandler(gamepadIndex) {
+    console.log(`Gamepad ${gamepadIndex} connected !`)
+  }
+
+  disconnectHandler(gamepadIndex) {
+    console.log(`Gamepad ${gamepadIndex} disconnected !`)
+  }
+
+   buttonChangeHandler(buttonName, down) {
+    console.log(buttonName, down)
+  }
   // COntroller inputs ==============================================================================
-  gamepad = () => {
-  	var start;
-  	var interval;
-  	var valid;
 
-  // 	window.addEventListener("gamepadconnected", function(e) {
-		//   var gp = navigator.getGamepads()[e.gamepad.index];
-		//   let gamepadInfo = "Gamepad connected at index " + gp.index + ": " + gp.id + ". It has " + gp.buttons.length + " buttons and " + gp.axes.length + " axes.";
-		//   console.log(gamepadInfo);
+ //  gamepad = () => {
+ //  	var start;
+ //  	var interval;
+ //  	var valid;
 
-		//   gameLoop();
-		// });
+ //  // 	window.addEventListener("gamepadconnected", function(e) {
+	// 	//   var gp = navigator.getGamepads()[e.gamepad.index];
+	// 	//   let gamepadInfo = "Gamepad connected at index " + gp.index + ": " + gp.id + ". It has " + gp.buttons.length + " buttons and " + gp.axes.length + " axes.";
+	// 	//   console.log(gamepadInfo);
 
-		window.addEventListener("gamepaddisconnected", function(e) {
-		  let gamepadInfo = "Waiting for gamepad.";
-		  console.log(gamepadInfo);
+	// 	//   gameLoop();
+	// 	// });
 
-		  // cancelRequestAnimationFrame(start);
-		});
+	// 	window.addEventListener("gamepaddisconnected", function(e) {
+	// 	  let gamepadInfo = "Waiting for gamepad.";
+	// 	  console.log(gamepadInfo);
+
+	// 	  // cancelRequestAnimationFrame(start);
+	// 	});
 
 
 
-		if (!('ongamepadconnected' in window)) {
-		  // No gamepad events available, poll instead.
-		  console.log("Checking for gamepads");
-		  interval = setInterval(pollGamepads, 500);
-		}
+	// 	if (!('ongamepadconnected' in window)) {
+	// 	  // No gamepad events available, poll instead.
+	// 	  console.log("Checking for gamepads");
+	// 	  interval = setInterval(pollGamepads, 500);
+	// 	}
 
-		function validGamepad(i) {
+	// 	function validGamepad(i) {
 
-			var gamepads = navigator.getGamepads ? navigator.getGamepads() : (navigator.webkitGetGamepads ? navigator.webkitGetGamepads : []);
+	// 		var gamepads = navigator.getGamepads ? navigator.getGamepads() : (navigator.webkitGetGamepads ? navigator.webkitGetGamepads : []);
 
-		  var gp = gamepads[i];
-		  // console.log(gp);
-    	if (gp.id.slice(0, 15) !== "Unknown Gamepad") {
-    		console.log("Valid gamepad detected");
-    		valid = i;
-    		gameLoop();
-    	}
+	// 	  var gp = gamepads[i];
+	// 	  // console.log(gp);
+ //    	if (gp.id.slice(0, 15) !== "Unknown Gamepad") {
+ //    		console.log("Valid gamepad detected");
+ //    		valid = i;
+ //    		gameLoop();
+ //    	}
 
-		  console.log("Gamepad invalid");
-		  return false
-		}
+	// 	  console.log("Gamepad invalid");
+	// 	  return false
+	// 	}
 
-		function pollGamepads() {
-		  var gamepads = navigator.getGamepads ? navigator.getGamepads() : (navigator.webkitGetGamepads ? navigator.webkitGetGamepads : []);
-		  for (var i = 0; i < gamepads.length; i++) {
-		    var gp = gamepads[i];
-		    if (gp) {
-		      let gamepadInfo = "Gamepad connected at index " + gp.index + ": " + gp.id +
-		        ". It has " + gp.buttons.length + " buttons and " + gp.axes.length + " axes.";
+	// 	function pollGamepads() {
+	// 	  var gamepads = navigator.getGamepads ? navigator.getGamepads() : (navigator.webkitGetGamepads ? navigator.webkitGetGamepads : []);
+	// 	  for (var i = 0; i < gamepads.length; i++) {
+	// 	    var gp = gamepads[i];
+	// 	    if (gp) {
+	// 	      let gamepadInfo = "Gamepad connected at index " + gp.index + ": " + gp.id +
+	// 	        ". It has " + gp.buttons.length + " buttons and " + gp.axes.length + " axes.";
 
-		      console.log(gamepadInfo);
-			  	validGamepad(i);
-			  	clearInterval(interval);
+	// 	      console.log(gamepadInfo);
+	// 		  	validGamepad(i);
+	// 		  	clearInterval(interval);
 				  
-		    }
-		  }
+	// 	    }
+	// 	  }
 
 
-		}
+	// 	}
 
-		function buttonPressed(b) {
-		  if (typeof(b) == "object") {
-		    return b.pressed;
-		  }
-		  return b == 1.0;
-		}
+	// 	function buttonPressed(b) {
+	// 	  if (typeof(b) == "object") {
+	// 	    return b.pressed;
+	// 	  }
+	// 	  return b == 1.0;
+	// 	}
 
-		function gameLoop() {
-		  var gamepads = navigator.getGamepads ? navigator.getGamepads() : (navigator.webkitGetGamepads ? navigator.webkitGetGamepads : []);
-		  //if can't find gamepads without Unknown Gamepad
-		  if (!gamepads[valid]) {
-		  	console.log("game loop stopped.");
-		    return;
-		  }
+	// 	function gameLoop() {
+	// 	  var gamepads = navigator.getGamepads ? navigator.getGamepads() : (navigator.webkitGetGamepads ? navigator.webkitGetGamepads : []);
+	// 	  //if can't find gamepads without Unknown Gamepad
+	// 	  if (!gamepads[valid]) {
+	// 	  	console.log("game loop stopped.");
+	// 	    return;
+	// 	  }
 	  
 
-		  var gp = gamepads[valid];
-		  if (buttonPressed(gp.buttons[0])) {
-		    console.log("Button 0 pressed");
-		  } else if (buttonPressed(gp.buttons[2])) {
-		    console.log("Button 2 pressed");
-		  }
-		  if (buttonPressed(gp.buttons[1])) {
-		    console.log("Button 3 pressed");
-		  } else if (buttonPressed(gp.buttons[3])) {
-		    console.log("Button 4 pressed");
-		  }
+	// 	  var gp = gamepads[valid];
+	// 	  if (buttonPressed(gp.buttons[0])) {
+	// 	    console.log("Button 0 pressed");
+	// 	  } else if (buttonPressed(gp.buttons[2])) {
+	// 	    console.log("Button 2 pressed");
+	// 	  }
+	// 	  if (buttonPressed(gp.buttons[1])) {
+	// 	    console.log("Button 3 pressed");
+	// 	  } else if (buttonPressed(gp.buttons[3])) {
+	// 	    console.log("Button 4 pressed");
+	// 	  }
 
-		  start = requestAnimationFrame(gameLoop);
-		}
+	// 	  start = requestAnimationFrame(gameLoop);
+	// 	}
 
-  }
- //  gamepadListener = () => {
+ //  }
+ // //  gamepadListener = () => {
 
 	// 	window.addEventListener("gamepadconnected", function(e) {
 	// 	  var gp = navigator.getGamepads()[e.gamepad.index];
@@ -288,6 +301,41 @@ class Combo extends Component {
 				/>
 
 
+
+
+			<Gamepad
+        onConnect={this.connectHandler}
+        onDisconnect={this.disconnectHandler}
+
+        onButtonChange={this.buttonChangeHandler}
+
+         gamepadIndex={0}
+      >
+      	<br />
+      </ Gamepad>
+			<Gamepad
+        onConnect={this.connectHandler}
+        onDisconnect={this.disconnectHandler}
+
+        onButtonChange={this.buttonChangeHandler}
+
+         gamepadIndex={1}
+      >
+      	<br />
+      </ Gamepad>
+			<Gamepad
+        onConnect={this.connectHandler}
+        onDisconnect={this.disconnectHandler}
+
+        onButtonChange={this.buttonChangeHandler}
+
+         gamepadIndex={2}
+      >
+      	<br />
+      </ Gamepad>
+
+
+    
 
 
 			</div>
---
+++ client/src/utils/gamepad.js
@@ -0,0 +1,18 @@
+import createJoyMap, { createQueryModule } from 'joymap';
+// import createJoyMap, { createEventModule } from 'joymap';
+
+export default {
+	joyevent() {
+		// const joyMap = createJoyMap();
+		// const module = createEventModule();
+
+		// joyMap.addModule(module);
+		// joyMap.addButtonEvent('A + B + !X + !Y', () => console.log('A & B pressed at the same time but not X nor Y'));
+		// joyMap.addButtonEvent('X + X.justChanged + Y', () => console.log('X is pressed & justChanged and Y is pressed'));
+		// joyMap.addButtonEvent('B + !B.justChanged', () => console.log('B pressed but not changed'));
+		// joyMap.start();
+	},
+	testfile()  {
+		console.log("Hello World");
+	}
+};
