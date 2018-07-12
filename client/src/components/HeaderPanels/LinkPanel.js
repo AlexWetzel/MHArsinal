89d35e932f3e913fa19dcc325503f825807267d1
AlexWetzel <wetzela@live.com>
2018-07-12 19:04:35 -0400

    Added Link tag to Link panels

--- client/src/components/HeaderPanels/LinkPanel.js
+++ client/src/components/HeaderPanels/LinkPanel.js
@@ -1,14 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './LinkPanel.css';
 
 export const LinkPanel = (props) =>
 
 	<div className="col s12 m6">
-		<a href={props.link}>
+		<Link to={props.link}>
 			<div id={props.id} className="link-panel">
-
 				<h3 className="center-align">{props.title}</h3>
-
 			</div>
-		</a>
+		</Link>
 	</div>;
