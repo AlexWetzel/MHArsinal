8e8539cbfcc62f28975b92abfff9c2c030432040
AlexWetzel <wetzela@live.com>
2018-07-12 18:55:42 -0400

    Added link component to nav bar to prevent refreshes

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,33 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import "./Nav.css";
 
 const Nav = () =>
 
   <div  className="navbar-fixed">
     <ul id="dropdown1" className="dropdown-content">
-      <li><a href="/weapons">Weapons Overview</a></li>
+      <li><Link to="/weapons">Weapons Overview</Link></li>
       <li className="divider"></li>
-      <li><a href="/greatsword">Great Sword</a></li>
-      <li><a href="/longsword">Long Sword</a></li>
-      <li><a href="/swordandshield">Sword & Shield</a></li>
-      <li><a href="/dualblades">Dual Blades</a></li>
-      <li><a href="/hammer">Hammer</a></li>
-      <li><a href="/huntinghorn">Hunting Horn</a></li>
-      <li><a href="/lance">Lance</a></li>
-      <li><a href="/gunlance">Gunlance</a></li>
-      <li><a href="/switchaxe">Switch Axe</a></li>
-      <li><a href="/chargeblade">Charge Blade</a></li>
-      <li><a href="/insectglaive">Insect Glaive</a></li>
-      <li><a href="/lightbowgun">Light Bowgun</a></li>
-      <li><a href="/heavybowgun">Heavy Bowgun</a></li>
-      <li><a href="/bow">Bow</a></li>
+      <li><Link to="/greatsword">Great Sword</Link></li>
+      <li><Link to="/longsword">Long Sword</Link></li>
+      <li><Link to="/swordandshield">Sword & Shield</Link></li>
+      <li><Link to="/dualblades">Dual Blades</Link></li>
+      <li><Link to="/hammer">Hammer</Link></li>
+      <li><Link to="/huntinghorn">Hunting Horn</Link></li>
+      <li><Link to="/lance">Lance</Link></li>
+      <li><Link to="/gunlance">Gunlance</Link></li>
+      <li><Link to="/switchaxe">Switch Axe</Link></li>
+      <li><Link to="/chargeblade">Charge Blade</Link></li>
+      <li><Link to="/insectglaive">Insect Glaive</Link></li>
+      <li><Link to="/lightbowgun">Light Bowgun</Link></li>
+      <li><Link to="/heavybowgun">Heavy Bowgun</Link></li>
+      <li><Link to="/bow">Bow</Link></li>
     </ul>
     <nav>
       <div className="nav-wrapper">
         <ul id="nav-mobile" className="hide-on-med-and-down">
-          <li><a className="nav-button brand" href="/">MONSTER HUNTER ARMS</a></li>
+          <li><Link className="nav-button brand" to="/">MONSTER HUNTER ARMS</Link></li>
           <li><a className="nav-button dropdown-button center-align" href="#!" data-activates="dropdown1">Weapons<i className="material-icons right">arrow_drop_down</i></a></li>
-          <li><a className="nav-button"href="/combo">Combo Test</a></li>
+          <li><Link className="nav-button"to="/combo">Combo Test</Link></li>
         </ul>
       </div>
     </nav>
